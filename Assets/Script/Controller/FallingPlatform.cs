68a2faf3e369cb78e672c0d8fc9cdd80eada2a8a
EggUncle <cicadashadow@gmail.com>
2020-12-14 17:27:25 +0800

    Falling platfrom & Movable platform

---
+++ Assets/Script/Controller/FallingPlatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour {
+
+    public float fallingTime = 2;
+
+    private TargetJoint2D targetJoint2D;
+    private BoxCollider2D boxCollider2D;
+
+    // Start is called before the first frame update
+    void Start() {
+        targetJoint2D = GetComponent<TargetJoint2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag.Equals("Player")) {
+            Invoke(nameof(fall), fallingTime);
+        }
+        if (collision.gameObject.tag.Equals("StaticGround")) {
+            Destroy(gameObject);
+        }
+    }
+
+    private void fall() {
+        targetJoint2D.enabled = false;
+    }
+
+}
---
+++ Assets/Script/Controller/MovableGroundController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace odyssey {
+
+    public class MovableGroundController : MovableObject
+    {
+
+        private float topY;
+        private float bottomY;
+        public float speed = 100;
+
+        private bool moveUp;
+        private Rigidbody2D rigidbody2;
+
+
+        public override void Move() {
+            if (transform.position.y > topY) {
+                moveUp = false;
+            } else if (transform.position.y < bottomY) {
+                moveUp = true;
+            }
+            rigidbody2.velocity = new Vector2(rigidbody2.velocity.x, speed * Time.fixedDeltaTime * (moveUp ? 1 : -1));
+        }
+
+        // Start is called before the first frame update
+        void Start() {
+            Transform topTransform = gameObject.transform.GetChild(0);
+            Transform bottomTransform = gameObject.transform.GetChild(1);
+
+            topY = topTransform.position.y;
+            bottomY = bottomTransform.position.y;
+
+            Destroy(topTransform.gameObject);
+            Destroy(bottomTransform.gameObject);
+
+            rigidbody2 = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+
+}
